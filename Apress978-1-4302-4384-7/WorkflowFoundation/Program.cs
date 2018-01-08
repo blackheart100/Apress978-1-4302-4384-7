dcc3933cca2d073c18d6c4df03295a2875a908f3
blackheart100 <1290823140@qq.com>
2018-01-08 20:00:22 +0800

    32

---
+++ Apress978-1-4302-4384-7/WorkflowFoundation/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Linq;
+using System.Activities;
+using System.Activities.Statements;
+
+namespace WorkflowFoundation
+{
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Activity workflow1 = new ForEachAct();
+            WorkflowInvoker.Invoke(workflow1);
+            Console.ReadKey();
+        }
+    }
+}
